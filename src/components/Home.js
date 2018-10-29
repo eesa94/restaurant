5be0db19f9f39c9ba76254421046df9fff001ddb
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-10-29 16:50:18 +0000

    add carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,36 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import cafe from '../img/cafe.jpg'
 
 
 const Home = () => {
+
+  const styles = {
+    carouselBackground: {
+      backgroundImage: `url(${cafe})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      backgroundSize: 'cover'
+    }
+  };
+
   return (
-        <div className="container section-container home">
-            <h4 className="center">Home</h4>
+        <div className="section-container home">
+          <div id="homeCarousel" class="carousel slide" data-ride="false" data-interval="5000" data-pause="false">
+            <ol class="carousel-indicators">
+              <li data-target="#homeCarousel" data-slide-to="0" class="active"></li>
+              <li data-target="#homeCarousel" data-slide-to="1"></li>
+              <li data-target="#homeCarousel" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active" id="slide-1" style={styles.carouselBackground}>
+              </div>
+              <div class="carousel-item" id="slide-2">
+              </div>
+              <div class="carousel-item" id="slide-3">
+              </div>
+            </div>
+          </div>
         </div>
   )
 }
