af358f6fcb3d916dbd3fd4dede2cdba9c1262fcd
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-10-29 17:23:50 +0000

    three carousel images; class => className

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import cafe from '../img/cafe.jpg'
+import office from '../img/office.jpg'
+import night from '../img/night.jpg'
 
 
 const Home = () => {
 
   const styles = {
-    carouselBackground: {
+    backgroundOne: {
       backgroundImage: `url(${cafe})`,
+    },
+
+    backgroundTwo: {
+      backgroundImage: `url(${office})`,
+    },
+
+    backgroundThree: {
+      backgroundImage: `url(${night})`,
+    },
+
+    backgroundProperties: {
       backgroundRepeat: 'no-repeat',
       backgroundPosition: 'center',
       backgroundSize: 'cover'
@@ -16,18 +29,18 @@ const Home = () => {
 
   return (
         <div className="section-container home">
-          <div id="homeCarousel" class="carousel slide" data-ride="false" data-interval="5000" data-pause="false">
-            <ol class="carousel-indicators">
-              <li data-target="#homeCarousel" data-slide-to="0" class="active"></li>
+          <div id="homeCarousel" className="carousel slide carousel-fade" data-ride="carousel" data-interval="5000" data-pause="false">
+            <ol className="carousel-indicators">
+              <li data-target="#homeCarousel" data-slide-to="0" className="active"></li>
               <li data-target="#homeCarousel" data-slide-to="1"></li>
               <li data-target="#homeCarousel" data-slide-to="2"></li>
             </ol>
-            <div class="carousel-inner">
-              <div class="carousel-item active" id="slide-1" style={styles.carouselBackground}>
+            <div className="carousel-inner">
+              <div className="carousel-item active" id="slide-1" style={{...styles.backgroundProperties, ...styles.backgroundOne}}>
               </div>
-              <div class="carousel-item" id="slide-2">
+              <div className="carousel-item" id="slide-2" style={{...styles.backgroundProperties, ...styles.backgroundTwo}}>
               </div>
-              <div class="carousel-item" id="slide-3">
+              <div className="carousel-item" id="slide-3" style={{...styles.backgroundProperties, ...styles.backgroundThree}}>
               </div>
             </div>
           </div>
