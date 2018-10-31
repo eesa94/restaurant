cd6c322e17044fc561fd5eb312d17ee4cb291671
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-10-31 09:00:40 +0000

    complete menu content

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -101,10 +101,79 @@ const Menu = () => {
                 <div className="row menu-row no-gutters">
                     <div className="col-10 offset-1 col-lg-4 offset-lg-3">
                         <p className="dish-name kodchasan-3"><strong>Beef Bulgogi</strong></p>
-                        <p className="dish-desc">Tender beef slices marinated in a delicious sauce</p>
+                        <p className="dish-desc">Tender beef slices marinated in our special sauce</p>
                     </div>
                     <div className="col-lg-1">
                         <p className="dish-price">£12.99</p>
+                    </div>
+                </div>
+                <div className="row menu-row no-gutters">
+                    <div className="col-10 offset-1 col-lg-4 offset-lg-3">
+                        <p className="dish-name kodchasan-3"><strong>Chicken Bulgogi</strong></p>
+                        <p className="dish-desc">Tender chicken strips marinated in our special sauce</p>
+                    </div>
+                    <div className="col-lg-1">
+                        <p className="dish-price">£10.99</p>
+                    </div>
+                </div>
+                <div className="row menu-row no-gutters">
+                    <div className="col-10 offset-1 col-lg-4 offset-lg-3">
+                        <p className="dish-name kodchasan-3"><strong>Spicy Chicken Bulgogi</strong></p>
+                        <p className="dish-desc">Tender chicken breast marinated in a spicy sauce</p>
+                    </div>
+                    <div className="col-lg-1">
+                        <p className="dish-price">£10.99</p>
+                    </div>
+                </div>
+                <div className="row menu-row no-gutters">
+                    <div className="col-10 offset-1 col-lg-4 offset-lg-3">
+                        <p className="dish-name kodchasan-3"><strong>Galbi</strong></p>
+                        <p className="dish-desc">Korean-style beef ribs marinated in traditional sauce</p>
+                    </div>
+                    <div className="col-lg-1">
+                        <p className="dish-price">£14.99</p>
+                    </div>
+                </div>
+            </div>
+
+            {/* Specials */}
+            <div className="menu-section">
+                <h2 className="menu-sub-header kodchasan-7">Specials</h2>
+                <div className="header-underline"></div>
+                <div className="row menu-row no-gutters">
+                    <div className="col-10 offset-1 col-lg-4 offset-lg-3">
+                        <p className="dish-name kodchasan-3"><strong>Bibimbap</strong></p>
+                        <p className="dish-desc">Tender beef slices served with rice, carrot, spinach, radish, sprouts, and fried egg</p>
+                    </div>
+                    <div className="col-lg-1">
+                        <p className="dish-price">£8.99</p>
+                    </div>
+                </div>
+                <div className="row menu-row no-gutters">
+                    <div className="col-10 offset-1 col-lg-4 offset-lg-3">
+                        <p className="dish-name kodchasan-3"><strong>Kimchi Fried Rice</strong></p>
+                        <p className="dish-desc">Traditional Korean Kimchi served with fried rice and egg</p>
+                    </div>
+                    <div className="col-lg-1">
+                        <p className="dish-price">£7.99</p>
+                    </div>
+                </div>
+                <div className="row menu-row no-gutters">
+                    <div className="col-10 offset-1 col-lg-4 offset-lg-3">
+                        <p className="dish-name kodchasan-3"><strong>Kimchi Stew</strong></p>
+                        <p className="dish-desc">Kimchi served with tofu, chilli pepper, onion</p>
+                    </div>
+                    <div className="col-lg-1">
+                        <p className="dish-price">£8.99</p>
+                    </div>
+                </div>
+                <div className="row menu-row no-gutters">
+                    <div className="col-10 offset-1 col-lg-4 offset-lg-3">
+                        <p className="dish-name kodchasan-3"><strong>Dumpling Soup</strong></p>
+                        <p className="dish-desc">Ox bone broth, served with Korean dumplings, tender beef slices, egg, onion, and seaweed</p>
+                    </div>
+                    <div className="col-lg-1">
+                        <p className="dish-price">£9.99</p>
                     </div>
                 </div>
             </div>
