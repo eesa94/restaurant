2d985767d1cd2ad1a612d5e92a5620420270ce43
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-11-03 17:47:07 +0000

    modify about section; change mobile menu width

--- src/components/About.js
+++ src/components/About.js
@@ -27,7 +27,6 @@ const About = () => {
                     <div className="col-10 offset-1 col-lg-8 offset-lg-2">
                         <p className="kodchasan-3">Seoul Food. was borne out of a passion for high-quality, authentic Korean cuisine. Noticing the popularity of East Asian establishments among the local populace, we wanted to bring another option to the table for local diners to satisfy their palate.</p>
                         <p className="kodchasan-3">Whether you are a seasoned veteran of the Korean BBQ, or you want to try Korean food for the first time, we have a wide range of dishes on offer which we are sure you will love!</p>
-                        <p className="kodchasan-3">Why not check out our menu and see what takes your fancy?</p>
                     </div>
                 </div>
             </div>
@@ -38,7 +37,7 @@ const About = () => {
                     <div className="col-10 offset-1 col-lg-8 offset-lg-2">
                         <p className="kodchasan-3">We like to keep things simple but effective.</p>
                         <p className="kodchasan-3">
-                        <span className="brand-tagline font-italic kodchasan-5">Taste Korea</span> is our motto. The entire Seoul Food. brand is centered around providing the most authentic taste of Korea that we possibly can. Everything from our ingredients to the restaurant environment is carefully designed to ensure that customers enjoy their experience.
+                        <strong><em>Taste Korea</em></strong> is our motto. The entire Seoul Food. brand is centered around providing the most authentic taste of Korea that we possibly can. Everything from our ingredients to the restaurant environment is carefully designed to ensure that customers enjoy their experience.
                         </p>
                         <p className="kodchasan-3">Check out our
                             <a href="/menu" className="menu-link"> menu </a>  
