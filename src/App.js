ab362ec94609b788428ff3cd847ee4a381a2de20
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-11-03 18:15:45 +0000

    scrollToTop component

--- src/App.js
+++ src/App.js
@@ -6,21 +6,24 @@ import About from './components/About';
 import Contact from './components/Contact';
 import Post from './components/Post';
 import Menu from './components/Menu';
+import ScrollToTop from './components/ScrollToTop';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='/about' component={About} />
-            <Route path='/menu' component={Menu} />
-            <Route path='/contact' component={Contact} />
-            <Route path="/:post_id" component={Post} />
-          </Switch>
-        </div>
+      <BrowserRouter onUpdate={() => window.scrollTo(0,0)}>
+        <ScrollToTop>
+          <div className="App">
+            <Navbar />
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/about' component={About} />
+              <Route path='/menu' component={Menu} />
+              <Route path='/contact' component={Contact} />
+              <Route path="/:post_id" component={Post} />
+            </Switch>
+          </div>
+        </ScrollToTop>
       </BrowserRouter>
     );
   }
