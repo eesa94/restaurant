add0ce38f90cae71bb2c581b34b47eea89bba9b6
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-11-03 14:57:00 +0000

    change navbar to class component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { Link, NavLink, withRouter } from 'react-router-dom';
 
-const Navbar = () => {
-    return (
-        <div>
-            <nav className="navigation text-center d-none d-lg-flex flex-column align-items-center justify-content-between">
-                <div>
-                    <a href="/" className="brand kodchasan-7 text-uppercase">Seoul Food.</a>
-                    <p className="brand-tagline text-right font-italic kodchasan-5">Taste Korea</p>
-                    <div className="underline"></div>
-                </div>
-                <ul className="link-list">
-                    <li><Link to="/" className="link">Home</Link></li>
-                    <li><NavLink to="/about" className="link">About</NavLink></li>
-                    <li><NavLink to="/menu" className="link">Menu</NavLink></li>
-                    <li><NavLink to="/contact" className="link">Contact</NavLink></li>
-                </ul>
-                <p className="copyright kodchasan-5">Copyright &copy; Seoul Food. 2018</p>
-            </nav>
+class Navbar extends Component {
+    constructor() {
+        super();
+        this.state = { navOpen: false};
+        this.navOpen = this.navOpen.bind(this);
+    }
 
-            <nav className="mobile-navigation text-center d-flex align-items-center justify-content-between d-lg-none">
-                <div className="brand-container">
-                    <a href="/" className="brand kodchasan-7 text-uppercase">Seoul Food.</a>
-                    <p className="brand-tagline text-right font-italic kodchasan-5">Taste Korea</p>
-                    <div className="underline"></div>
-                </div>
-            </nav>
-        </div>
-    )
+    navOpen = () => {
+
+        this.setState({
+            navOpen: true
+        });
+    }
+    
+    render() {
+        return (
+            <div>
+                <nav className="navigation text-center d-none d-lg-flex flex-column align-items-center justify-content-between">
+                    <div>
+                        <a href="/" className="brand kodchasan-7 text-uppercase">Seoul Food.</a>
+                        <p className="brand-tagline text-right font-italic kodchasan-5">Taste Korea</p>
+                        <div className="underline"></div>
+                    </div>
+                    <ul className="link-list">
+                        <li><Link to="/" className="link">Home</Link></li>
+                        <li><NavLink to="/about" className="link">About</NavLink></li>
+                        <li><NavLink to="/menu" className="link">Menu</NavLink></li>
+                        <li><NavLink to="/contact" className="link">Contact</NavLink></li>
+                    </ul>
+                    <p className="copyright kodchasan-5">Copyright &copy; Seoul Food. 2018</p>
+                </nav>
+
+                <nav className="mobile-navigation text-center d-flex align-items-center justify-content-between d-lg-none">
+                    <div className="brand-container">
+                        <a href="/" className="brand kodchasan-7 text-uppercase">Seoul Food.</a>
+                        <p className="brand-tagline text-right font-italic kodchasan-5">Taste Korea</p>
+                        <div className="underline"></div>
+                    </div>
+                    <button className="btn nav-toggle-btn" onClick={this.navOpen}>Click</button>
+                </nav>
+
+
+                { 
+                    this.state.navOpen ? (               
+                        <div className="mobile-menu">
+                            <p>hey dere</p>
+                        </div>
+                    ) : (
+                        null
+                    ) 
+                }
+            </div>
+        )
+    }
 }
 
 export default withRouter(Navbar)
