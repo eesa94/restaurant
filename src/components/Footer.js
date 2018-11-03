247e9fd1330cf09023e4393fc67b85c733f7331c
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-11-03 13:24:53 +0000

    mobile nav bar; brand font size mq

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,8 @@ import React from 'react'
 const Footer = () => {
     return (
         <div>
-            <footer className="d-flex align-items-center justify-content-between d-lg-none w-100">
+            <footer className="d-flex align-items-center justify-content-center d-lg-none w-100">
             <p className="copyright kodchasan-5">Copyright &copy; Seoul Food. 2018</p>
-
             </footer>
         </div>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,20 +3,30 @@ import { Link, NavLink, withRouter } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className="navigation text-center d-none d-lg-flex flex-column align-items-center justify-content-between">
-            <div>
-                <a href="/" className="brand kodchasan-7 text-uppercase">Seoul Food.</a>
-                <p className="brand-tagline text-right font-italic kodchasan-5">Taste Korea</p>
-                <div className="underline"></div>
-            </div>
-            <ul className="link-list">
-                <li><Link to="/" className="link">Home</Link></li>
-                <li><NavLink to="/about" className="link">About</NavLink></li>
-                <li><NavLink to="/menu" className="link">Menu</NavLink></li>
-                <li><NavLink to="/contact" className="link">Contact</NavLink></li>
-            </ul>
-            <p className="copyright kodchasan-5">Copyright &copy; Seoul Food. 2018</p>
-        </nav>
+        <div>
+            <nav className="navigation text-center d-none d-lg-flex flex-column align-items-center justify-content-between">
+                <div>
+                    <a href="/" className="brand kodchasan-7 text-uppercase">Seoul Food.</a>
+                    <p className="brand-tagline text-right font-italic kodchasan-5">Taste Korea</p>
+                    <div className="underline"></div>
+                </div>
+                <ul className="link-list">
+                    <li><Link to="/" className="link">Home</Link></li>
+                    <li><NavLink to="/about" className="link">About</NavLink></li>
+                    <li><NavLink to="/menu" className="link">Menu</NavLink></li>
+                    <li><NavLink to="/contact" className="link">Contact</NavLink></li>
+                </ul>
+                <p className="copyright kodchasan-5">Copyright &copy; Seoul Food. 2018</p>
+            </nav>
+
+            <nav className="mobile-navigation text-center d-flex align-items-center justify-content-between d-lg-none">
+                <div className="brand-container">
+                    <a href="/" className="brand kodchasan-7 text-uppercase">Seoul Food.</a>
+                    <p className="brand-tagline text-right font-italic kodchasan-5">Taste Korea</p>
+                    <div className="underline"></div>
+                </div>
+            </nav>
+        </div>
     )
 }
 
